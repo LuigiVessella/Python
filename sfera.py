b3fbd4386238ca864755aa8896214f4c39eb3efb
LuigiVessella <vessellaluigi12@gmail.com>
2016-11-03 17:09:21 +0100

    Sfera modificato

--- sfera.py
+++ sfera.py
@@ -1,6 +1,7 @@
-pi = 3.141592654
-r= input("Inserisci il raggio: ")
-area = 4*pi*r*r
-volume = (4*3.14*r*r*r)/3
+import math
+
+r=float( input("Inserisci il raggio: "))
+area = 4*math.pi*r*r
+volume = (4*math.pi*r*r*r)/3
 print "L'area della superfice e'",area
 print "Il volume della sfera e'",volume
