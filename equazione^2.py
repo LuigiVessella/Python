ede1cd219a4c8cd8a136667fcdbc932dd701dd34
LuigiVessella <vessellaluigi12@gmail.com>
2016-11-05 18:03:15 +0100

    Modifiche

--- equazione^2.py
+++ equazione^2.py
@@ -2,21 +2,22 @@ import math
 termX2=input("Inserisci il termine in x^2: ")
 termX=input("Inserisci il temrine in x^1: ")
 termN=input("Inserisci il termine noto: ")
-if termX2 != 0 and termX != 0 and  termN != 0:
-   delta=pow(termX,2) - 4.0*(termX2*termN)
-   ris=(((termX*(-1))+ math.sqrt(delta)))/(2*termX2)
-   ris2=(((termX*(-1))- math.sqrt(delta)))/(2*termX2)
-   print "Le soluzioni sono: ",ris, " ", ris2
+if termX==0 and termN==0: 
+   print "L'equazione e' impossibile. "
+
+elif termN == 0:
+   ris1 = 0
+   ris2 = (termX/termX2)*(-1.0)
+   print "Le soluzioni sono: ", ris1, " ", ris2
 
 elif termX == 0:
-   ris = math.sqrt(termN/termX2)
-
-   
-elif termN == 0:
-   ris= 0.0
-   ris2=  termX2/termX * (-1.0)
-   print "Le soluzioni sono: ", ris, " ", ris2
+   ris = math.sqrt((termX2/termN)*(-1.0))
+   print "La soluzione e': ", ris
 
 else:
-    print "E' impossible. "
+    delta=pow(termX,2) - 4.0*(termX2*termN)
+    ris=(((termX*(-1))+ math.sqrt(delta)))/(2*termX2)
+    ris2=(((termX*(-1))- math.sqrt(delta)))/(2*termX2)
+    print "Le soluzioni sono: ",ris, " ", ris2
+
 
