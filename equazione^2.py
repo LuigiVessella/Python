0d5bc259f63e08883fa7d90f0578f8e43d07e961
LuigiVessella <vessellaluigi12@gmail.com>
2016-11-06 18:54:00 +0100

    Modifiche

--- equazione^2.py
+++ equazione^2.py
@@ -5,25 +5,28 @@ termX2=input("Inserisci il coefficiente di x^2: ")
 termX=input("Inserisci il coefficiente di  x^1: ")
 termN=input("Inserisci il termine noto: ")
 
-if termX2 == 0 and termX==0 and termN == 0:
+if termX2 == 0 and termX==0 and termN == 0: 
    print "Non e' un'equazione valida"
 
-elif termX==0 and termN==0: 
-   print "E' un equazione monomia ax^2=0, e' verificata solo per x=0 "
-
-elif termX == 0:
-   ris = math.sqrt(((termN/termX2)*(-1.0)))
-   print "La soluzione e': ", ris
+elif termX2==0 and termX==0:
+   print termN , "= 0 "
 
 elif termX2==0:
-    ris =float (termN) / float (termX) * (-1.0)
+    ris = float (termN) / float (termX) * (-1.0)
     print "La soluzione dell'equazione e' x = ",ris
 
 else:
-    delta=pow(termX,2) - 4.0*(termX2*termN)
-    ris=(((termX*(-1))+ math.sqrt(delta)))/(2*termX2)
-    ris2=(((termX*(-1))- math.sqrt(delta)))/(2*termX2)
-    print "Le soluzioni sono: ",ris, "e ", ris2
+    delta =  pow(termX,2) - 4.0*(termX2*termN)
+    if delta < 0:
+         print "L'equazione e' impossibile perchè il delta è minore di 0. "
+         else: 
+            delta = pow(termX,2) - 4.0*(termX2*termN)
+            ris=(((termX*(-1))+ math.sqrt(delta)))/(2*termX2)
+            ris2=(((termX*(-1))- math.sqrt(delta)))/(2*termX2)
+            print "Le soluzioni sono: ",ris, "e ", ris2
+            
+         
+    
 
 
 
