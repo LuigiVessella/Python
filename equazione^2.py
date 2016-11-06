f1aa6820d58916c873b80f13762666e7862379ed
LuigiVessella <vessellaluigi12@gmail.com>
2016-11-06 17:57:58 +0100

    Modifiche

--- equazione^2.py
+++ equazione^2.py
@@ -11,23 +11,19 @@ if termX2 == 0 and termX==0 and termN == 0:
 elif termX==0 and termN==0: 
    print "E' un equazione monomia ax^2=0, e' verificata solo per x=0 "
 
-elif termN == 0:
-   ris1 = 0
-   ris2 = (termX/termX2)*(-1.0)
-   print "Le soluzioni sono: ", ris1, "e ", ris2
-
 elif termX == 0:
    ris = math.sqrt(((termN/termX2)*(-1.0)))
    print "La soluzione e': ", ris
 
 elif termX2==0:
-     ris =float (termN) / float (termX) * (-1.0)
-      print "La soluzione dell'equazione e' x = ",ris
+    ris =float (termN) / float (termX) * (-1.0)
+    print "La soluzione dell'equazione e' x = ",ris
 
 else:
     delta=pow(termX,2) - 4.0*(termX2*termN)
     ris=(((termX*(-1))+ math.sqrt(delta)))/(2*termX2)
     ris2=(((termX*(-1))- math.sqrt(delta)))/(2*termX2)
     print "Le soluzioni sono: ",ris, "e ", ris2
+
 
 
