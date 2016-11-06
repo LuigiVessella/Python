3250b6e4b038f083f5b39cffd787ba18a13f912c
LuigiVessella <vessellaluigi12@gmail.com>
2016-11-06 17:12:58 +0100

    Modifiche

--- equazione^2.py
+++ equazione^2.py
@@ -1,8 +1,14 @@
+#programma che risolve equazioni di 2 grado
+
 import math
 termX2=input("Inserisci il coefficiente di x^2: ")
 termX=input("Inserisci il coefficiente di  x^1: ")
 termN=input("Inserisci il termine noto: ")
-if termX==0 and termN==0: 
+
+if termX2 == 0 and termX==0 and termN == 0:
+   print "Non e' un'equazione valida"
+
+elif termX==0 and termN==0: 
    print "E' un equazione monomia ax^2=0, e' verificata solo per x=0 "
 
 elif termN == 0:
@@ -13,6 +19,10 @@ elif termN == 0:
 elif termX == 0:
    ris = math.sqrt(((termN/termX2)*(-1.0)))
    print "La soluzione e': ", ris
+
+elif termX2==0:
+     ris =float (termN) / float (termX) * (-1.0)
+      print "La soluzione dell'equazione e' x = ",ris
 
 else:
     delta=pow(termX,2) - 4.0*(termX2*termN)
