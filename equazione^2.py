54322de00fd1179a32b10738ca83b82ee4c17b00
LuigiVessella <vessellaluigi12@gmail.com>
2016-11-06 09:06:21 +0100

    Modifiche

--- equazione^2.py
+++ equazione^2.py
@@ -1,23 +1,23 @@
 import math
-termX2=input("Inserisci il termine in x^2: ")
-termX=input("Inserisci il temrine in x^1: ")
+termX2=input("Inserisci il coefficiente di x^2: ")
+termX=input("Inserisci il coefficiente di  x^1: ")
 termN=input("Inserisci il termine noto: ")
 if termX==0 and termN==0: 
-   print "L'equazione e' impossibile. "
+   print "E' un equazione monomia ax^2=0, e' verificata solo per x=0 "
 
-elif termN == 0:
+elif termN == 0 and termX !=0 and termX2 !=0:
    ris1 = 0
    ris2 = (termX/termX2)*(-1.0)
-   print "Le soluzioni sono: ", ris1, " ", ris2
+   print "Le soluzioni sono: ", ris1, "e ", ris2
 
-elif termX == 0:
-   ris = math.sqrt((termX2/termN)*(-1.0))
+elif termX == 0 and termX2 != 0 and termN != 0:
+   ris = math.sqrt(((termN/termX2)*(-1.0)))
    print "La soluzione e': ", ris
 
 else:
     delta=pow(termX,2) - 4.0*(termX2*termN)
     ris=(((termX*(-1))+ math.sqrt(delta)))/(2*termX2)
     ris2=(((termX*(-1))- math.sqrt(delta)))/(2*termX2)
-    print "Le soluzioni sono: ",ris, " ", ris2
+    print "Le soluzioni sono: ",ris, "e ", ris2
 
 
